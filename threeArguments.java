7550d44bc1fcc8d4418bd755a5395fc561c7a32e
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-11-29 15:44:43 -0600

    Create threeArguments.java
    Esta clase imprime tres argumentos proporcionados por el usuario.

---
+++ threeArguments.java
@@ -0,0 +1,12 @@
+/***************************************************************
+ * Este programa recibe tres argumentos y 
+ * los muestra en forma inversa a como los recibio originalmente.
+ ***************************************************************/
+ 
+public class threeArguments {
+	public static void main(String[] args){
+		
+		System.out.println("Hola tus palabras fueron: " + args[2] + ' ' + args[1] + ' ' + args[0]);
+		
+	}
+}
