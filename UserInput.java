7e324e07d8b56c5fada5dcc22c87f7067cff9007
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-11-28 21:56:43 -0600

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class UserInput {
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
 
     Scanner scn = new Scanner(System.in);
     String nombre = "";
