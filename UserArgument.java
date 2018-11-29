c325ad0fde7a6b5946538b6ab358256ca523034d
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-11-29 14:02:07 -0600

    Create UserArgument.java
    Esta clase utiliza un argumento capturado en la consola, para complementar su mensaje.

---
+++ UserArgument.java
@@ -0,0 +1,11 @@
+/***********************************************************
+ * Este ejemplo recibe como argumento el nombre de usuario.
+ ************************************************************/
+ 
+public class UserArgument {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola, " + args[0] + " como te encuentras hoy?");
+	}
+	
+}
