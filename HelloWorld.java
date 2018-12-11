72f4bad8472c11ac30dd57ba5e0a549557c65382
Fernando-Perulles <fernando.perulles@yahoo.com>
2018-12-11 13:00:17 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,12 @@
+/*******************************************************
+ * Compilación: javac HelloWorld.java
+ * Ejecución:   java HelloWorld
+ * Esta clase permite imprimir un texto en la consola.
+ *******************************************************/
+
 public class HelloWorld {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
 
     System.out.println("Hello World!");
 
