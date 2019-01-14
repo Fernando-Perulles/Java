6b80fe2955c5c8d5fb88105b30e41be6f7f44cbb
Fernando-Perulles <fernando.perulles@yahoo.com>
2019-01-14 00:23:43 -0600

    Create HelloYourName.java
    This hello sample, uses try catch error control.

---
+++ HelloWorld/HelloYourName.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; // import Scanner class for scan object declaration.
+
+public class HelloYourName { // public is the access modifier.
+	
+	public static void main (String[] args) {
+
+	System.out.print("What is your name? ");
+	Scanner scan = new Scanner(System.in);
+	String name = scan.nextLine();
+	
+		try {
+			if (name != "") {
+				
+				System.out.println("Hello " + name);
+				
+			}
+		}
+		catch (Exception e) {
+			System.out.println("" + e.getMessage());
+		}
+		
+	}
+	
+}
