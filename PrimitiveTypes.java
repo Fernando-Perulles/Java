069e96f9997cbd0fbcaf4251f8447dc9614d877a
Fernando-Perulles <fernando.perulles@yahoo.com>
2019-01-14 01:57:35 -0600

    Create PrimitiveTypes.java

---
+++ PrimitiveTypes.java
@@ -0,0 +1,45 @@
+public class PrimitiveTypes {
+
+  public static void main (String args[]) {
+  
+      System.out.println("In java the primitive types are: ");
+      System.out.print(Byte.TYPE + " " + "|");
+      System.out.print(Byte.SIZE + " " + "bits | from ");
+      System.out.print(Byte.MIN_VALUE + " " + "to ");
+      System.out.println(Byte.MAX_VALUE + " ");
+
+      System.out.print(Short.TYPE + " " + "|");
+      System.out.print(Short.SIZE + " " + "bits | from ");
+      System.out.print(Short.MIN_VALUE + " " + "to ");
+      System.out.println(Short.MAX_VALUE + " ");
+
+      System.out.print(Integer.TYPE + " " + "|");
+      System.out.print(Integer.SIZE + " " + "bits | from ");
+      System.out.print(Integer.MIN_VALUE + " " + "to ");
+      System.out.println(Integer.MAX_VALUE + " ");
+		
+      System.out.print(Long.TYPE + " " + "|");
+      System.out.print(Long.SIZE + " " + "bits | from ");
+      System.out.print(Long.MIN_VALUE + " " + "to ");
+      System.out.println(Long.MAX_VALUE + " ");
+
+      System.out.print(Float.TYPE + " " + "|");
+      System.out.print(Float.SIZE + " " + "bits | from ");
+      System.out.print(Float.MIN_VALUE + " " + "to ");
+      System.out.println(Float.MAX_VALUE + " ");
+
+      System.out.print(Double.TYPE + " " + "|");
+      System.out.print(Double.SIZE + " " + "bits | from ");
+      System.out.print(Double.MIN_VALUE + " " + "to ");
+      System.out.println(Double.MAX_VALUE + " ");
+		
+      System.out.print(Character.TYPE + " " + "|");
+      System.out.print(Character.SIZE + " " + "bits | from ");
+      System.out.print((int) Character.MIN_VALUE + " " + "to ");
+      System.out.println((int) Character.MAX_VALUE + " ");
+		
+      System.out.print(Boolean.TYPE + " ");
+      
+  }
+	
+}
