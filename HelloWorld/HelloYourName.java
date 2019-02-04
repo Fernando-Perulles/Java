43cb54a92fc0ddf906822338e0c1adee32876fb1
Fernando-Perulles <fernando.perulles@yahoo.com>
2019-02-04 17:58:51 -0600

    Update HelloYourName.java

--- HelloWorld/HelloYourName.java
+++ HelloWorld/HelloYourName.java
@@ -3,20 +3,21 @@
  * Execution: java HelloYourName
  * This program asks for your name and take it as parameter to print it into the console.
  **/
+
 import java.util.Scanner; // import Scanner class for scan object declaration.
 
-public class HelloYourName { // public is the access modifier.
+public class HelloYourName { // Class declaration. (public) is the access modifier.
 	
-	public static void main (String[] args) {
+	public static void main (String[] args) { // (main) class declaration.
 
-	System.out.print("What is your name? ");
-	Scanner scan = new Scanner(System.in);
-	String name = scan.nextLine();
+	System.out.print("What is your name? "); // Print message to console
+	Scanner scan = new Scanner(System.in); // new Scanner object declaration.
+	String name = scan.nextLine(); // String variable declaration. Assign the input to the String variable.
 	
 		try {
 			if (name != "") {
 				
-				System.out.println("Hello " + name);
+				System.out.println("Hello " + name); // Outpunt message to console with the input.
 				
 			}
 		}
